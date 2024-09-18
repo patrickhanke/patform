cbb407ac87c950cb4bc683c71db3b19312bd886a
Patrick Hanke <ph@patrickhanke.net>
2024-09-18 14:52:31 +0200

    ud

--- packages/modules/src/AppSettings/AppModules/AppModules.tsx
+++ packages/modules/src/AppSettings/AppModules/AppModules.tsx
@@ -1,43 +1,40 @@
 'use client';
 
-import { useContext } from 'react'
-import { AppContext, generateGraphQLQuery, paramsHandler } from '@repo/provider'
-import { DnDDisplay, Page, sortItemsByPosition } from '@repo/ui'
-import { Module } from '@repo/types'
-import { useQuery } from '@apollo/client'
+import { useContext } from 'react';
+import { AppContext, generateGraphQLQuery, paramsHandler } from '@repo/provider';
+import { DnDDisplay, Page, sortItemsByPosition } from '@repo/ui';
+import { Module } from '@repo/types';
+import { useQuery } from '@apollo/client';
 import AppModule from './content/AppModule';
 
 const AppModules = () => {
-    const {project} = useContext(AppContext)
+	const {project} = useContext(AppContext);
 
-    const {data, loading} = useQuery(generateGraphQLQuery(
-        {
-            type: 'find', 
-            objectName: 'Module', 
-            fields: ['objectId', 'name', 'createdAt', 'icon']
-        }
-    ), {
-        variables: paramsHandler({filters: [{key: 'project', value: project.objectId as string, operator: '_eq', id: 'projectId'}]} )
-    })
+	const {data, loading} = useQuery(generateGraphQLQuery(
+		{
+			type: 'find', 
+			objectName: 'Module', 
+			fields: ['objectId', 'name', 'createdAt', 'icon']
+		}
+	), {
+		variables: paramsHandler({filters: [{key: 'project', value: project.objectId as string, operator: '_eq', id: 'projectId'}]} )
+	});
 
-    if (loading) return null;
+	if (loading) return null;
 
-    const modules = data?.objects.findModule.results
+	const modules = data?.objects.findModule.results;
 
-    return (
-        <Page 
-            title='Module'
-            pageHeaderButtons={[{text: 'Neue Person erstellen', onClick: () => console.log(true)}]}
-            hasPageNavigation={true}
-            emptyContent={true}
-        >
-            <DnDDisplay
-                items={sortItemsByPosition(modules).map((module: Module) => ({...module, id: module.objectId})) || []}
-                ItemComponent={({item}) => (<AppModule id={item.id} />)}
-                objectClass='Module'
-            />
-    </Page>
-  )
-}
+	return (
+		<Page 
+			title='Module'
+		>
+			<DnDDisplay
+				items={sortItemsByPosition(modules).map((module: Module) => ({...module, id: module.objectId})) || []}
+				ItemComponent={({item}) => (<AppModule id={item.id} />)}
+				objectClass='Module'
+			/>
+		</Page>
+	);
+};
 
-export default AppModules
+export default AppModules;
