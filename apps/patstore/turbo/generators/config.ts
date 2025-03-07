5dfb4e50b6e17170aab38fae5581ed2a7c894e40
Patrick Hanke <ph@patrickhanke.net>
2025-03-07 13:07:29 +0100

    Update config.ts

--- apps/patstore/turbo/generators/config.ts
+++ apps/patstore/turbo/generators/config.ts
@@ -4,23 +4,23 @@ import type { PlopTypes } from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   // A simple generator to add a new React component to the internal UI library
-  const folderArray = [
-    {
-      name: "EmailHandler",
-      path: "email",
-    }
-  ]
-  folderArray.forEach((folder) => {
-    plop.setGenerator("app-routing", {
-      description: "Adds new Routes",
-      prompts: [],
-      actions: [
-        {
-          type: "add",
-          path: `app/(application)/${folder.name}/page.tsx`,
-          templateFile: "templates/component.hbs",
-        },
-      ],
-    });
-  });
+  // const folderArray = [
+  //   {
+  //     name: "EmailHandler",
+  //     path: "email",
+  //   }
+  // ]
+  // folderArray.forEach((folder) => {
+  //   plop.setGenerator("app-routing", {
+  //     description: "Adds new Routes",
+  //     prompts: [],
+  //     actions: [
+  //       {
+  //         type: "add",
+  //         path: `app/(application)/${folder.name}/page.tsx`,
+  //         templateFile: "templates/component.hbs",
+  //       },
+  //     ],
+  //   });
+  // });
 }
