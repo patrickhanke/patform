f30caddf5a5797c7697060539f5ddd2c0953e8e8
Patrick Hanke <ph@patrickhanke.net>
2025-03-11 18:26:08 +0100

    ud invite

--- apps/patstore/app/(login)/content/Invite/Invite.tsx
+++ apps/patstore/app/(login)/content/Invite/Invite.tsx
@@ -1,38 +1,41 @@
 'use client';
 
-import { useMemo } from 'react';
+import { FC, useMemo } from 'react';
 import './styles.scss';
 import { usePathname } from 'next/navigation';
 import { axiosclient } from '@repo/provider';
+import { PatstoreProject } from '@repo/types';
 
 const Invite = () => {
 	const path = usePathname();	
 	console.log(path);
 
-	const project = useMemo(() => {
-		let project;
+	const project: PatstoreProject | undefined = useMemo(() => {
+		let project: PatstoreProject | undefined = undefined;
 		if (path !== '/login') {
 			axiosclient().post('functions/get_project_from_path', {path})
-			.then(data => {
-				project = data;
+			.then((data) => {
+				project = data as unknown as PatstoreProject;
 	
 			})
 			.catch(error => console.error(error.message));
 		}
 		return project
 	}, [])
+
+	console.log({project});
+	
 	
 	return (
 		<>
-			<div className={'Invite_left_container'}>
-				<div className={'Invite_left_content'}>
+			<div className={'invite_container'}>
+				<div >
 					<h1>Patstore Invite</h1>
-					<div className={'Invite_divider'} />
 					<p>
-						Invite Patstore CMS
+						Sie wurden eingeladen, sich bei dem Projekt {project?.name || 'patstore'} anzumelden.
 					</p>
 				</div>
-				<div className={'Invite_footer'}>
+				<div >
 					<p>
 						Patstore
 					</p>
