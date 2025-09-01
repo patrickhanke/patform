fc20933aa3f0df901b59e8b4daa47125cd844921
Patrick Hanke <ph@patrickhanke.net>
2025-09-01 14:20:55 +0200

    Update ImageUploader.tsx

--- packages/ui/src/patstore/Displays/PatstoreImageUploader/content/SelectImage/components/ImageUploader.tsx
+++ packages/ui/src/patstore/Displays/PatstoreImageUploader/content/SelectImage/components/ImageUploader.tsx
@@ -1,130 +1,20 @@
 "use client";
 
-import { FC, useContext, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import "../styles.scss";
 
-import { ErrorDisplay } from "@repo/ui";
-import { PatstoreAppContext, useDataHandler } from "@repo/provider";
+import { FileUploader } from "@repo/ui";
 import { ImageUploaderProps } from "../types";
-import Uppy from "@uppy/core";
 // import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/css/style.min.css";
-import { Dashboard, UploadButton } from "@uppy/react";
-import German from "@uppy/locales/lib/de_DE";
-import { ImageClass, Module } from "@repo/types";
-import createUppyInstance from "../../../../../../general/Inputs/FileUploader/functions/createUppyInstance";
 
 const ImageUploader: FC<ImageUploaderProps> = ({
 	onComplete,
 	maxFileCount,
 	afterUploadHandler,
-	type = "create",
-	className,
-	classKey,
 	classId
 }) => {
-	const { modules } = useContext(PatstoreAppContext);
 	const [isComplete, setIsComplete] = useState(false);
-	const { createImage, addImage } = useDataHandler();
-	const [isUploading, setIsUploading] = useState(false);
-
-	const uppy = createUppyInstance(type, maxFileCount);
-	// const files = useUppyState(uppy, (state) => state.files);
-	// console.log({ files });
-
-	uppy.on("upload-success", (file, response) => {
-		console.log("Upload successful:", file?.name, response);
-	});
-
-	uppy.on("upload-error", (file, error, response) => {
-		console.error("Upload error:", file?.name, error, response);
-	});
-
-	const imageModule =
-		modules && modules.find((module: Module) => module.path === "/images");
-
-	useEffect(() => {
-		// Custom upload logic
-		const uploadSuccessHandler = async () => {
-			const files = uppy.getFiles();
-
-			const imageArray: string[] = [];
-			if (!isComplete) {
-				if (type === "create" && imageModule) {
-					for (const file of files) {
-						try {
-							await createImage({
-								file,
-								name: file.name as string,
-								moduleId: imageModule.objectId,
-								feedback: "Bild erfolgreich hochgeladen",
-								afterSaveHandler: (image: ImageClass) => {
-									imageArray.push(image.file.url);
-								}
-							});
-						} catch (error) {
-							console.error(error);
-						}
-					}
-				} else if (type === "add") {
-					for (const file of files) {
-						if (!className || !classKey || !classId) {
-							console.error(
-								"className or classKey or classId is not defined"
-							);
-							return;
-						}
-						try {
-							await addImage({
-								file,
-								className,
-								classKey,
-								classId,
-								feedback: "Bild erfolgreich hinzugefügt",
-								afterSaveHandler: (image: ImageClass) => {
-									imageArray.push(image.file.url);
-								}
-							});
-						} catch (error) {
-							console.error(error);
-						}
-					}
-				}
-
-				uppy.emit("complete", {
-					successful: uppy.getFiles().filter((f) => !f.error),
-					failed: uppy.getFiles().filter((f) => f.error)
-				});
-
-				setIsComplete(true);
-
-				if (afterUploadHandler) {
-					afterUploadHandler(imageArray);
-				}
-			}
-		};
-
-		uppy.on("upload", uploadSuccessHandler);
-
-		// Clean up by removing the listener
-		return () => {
-			uppy.off("upload", uploadSuccessHandler);
-		};
-	}, [uppy, isComplete]);
-
-	useEffect(() => {
-		const uploadStartHandler = () => setIsUploading(true);
-		const uploadCompleteHandler = () => setIsUploading(false);
-
-		uppy.on("upload", uploadStartHandler);
-		uppy.on("complete", uploadCompleteHandler);
-
-		return () => {
-			uppy.off("upload", uploadStartHandler);
-			uppy.off("complete", uploadCompleteHandler);
-			uppy.clear();
-		};
-	}, [uppy]);
 
 	if (isComplete) {
 		return (
@@ -151,20 +41,16 @@ const ImageUploader: FC<ImageUploaderProps> = ({
 	}
 
 	return (
-		<div className={"uppy_upload_container"}>
-			<Dashboard
-				theme="light"
-				uppy={uppy}
-				width={"100%"}
-				height="360px"
-				style={{ padding: "24px" }}
-				proudlyDisplayPoweredByUppy={false}
-				disabled={isUploading}
-				showProgressDetails={true}
-			/>
-			<UploadButton />
-			<ErrorDisplay id="uloader" errors={[]} />
-		</div>
+		<FileUploader
+			classKey="file"
+			className="Image"
+			classId={classId}
+			afterUploadHandler={afterUploadHandler}
+			maxFileCount={maxFileCount}
+			existingFiles={0}
+			type="image"
+			inline
+		/>
 	);
 };
 
