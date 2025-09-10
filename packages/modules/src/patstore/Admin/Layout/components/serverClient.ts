6d41804578f51db320b63c2e071f478dcfe88aa0
Patrick Hanke <ph@patrickhanke.net>
2025-09-10 09:37:37 +0200

    Update serverClient.ts

--- packages/modules/src/patstore/Admin/Layout/components/serverClient.ts
+++ packages/modules/src/patstore/Admin/Layout/components/serverClient.ts
@@ -3,27 +3,27 @@
 import { ApolloClient, HttpLink } from "@apollo/client";
 // import { InMemoryCache } from '@apollo/client-react-streaming';
 import {
-  registerApolloClient,
-  InMemoryCache,
+	registerApolloClient,
+	InMemoryCache
 } from "@apollo/experimental-nextjs-app-support";
 
 const serverClient = (appUrl: string, appId: string, masterKey: string) => {
-  const { getClient } = registerApolloClient(() => {
-    return new ApolloClient({
-      cache: new InMemoryCache(),
-      link: new HttpLink({
-        uri: appUrl,
-        headers: {
-          "X-Parse-Application-Id": appId || "",
-          // 'X-Parse-REST-API-Key': process.env.SASHIDO_REST_KEY || '',
-          "X-Parse-Master-Key": masterKey || "",
-          // 'X-Parse-Session-Token': token
-        },
-      }),
-    });
-  });
+	const { getClient } = registerApolloClient(() => {
+		return new ApolloClient({
+			cache: new InMemoryCache(),
+			link: new HttpLink({
+				uri: appUrl,
+				headers: {
+					"X-Parse-Application-Id": appId || "",
+					// 'X-Parse-REST-API-Key': process.env.SASHIDO_REST_KEY || '',
+					"X-Parse-Master-Key": masterKey || ""
+					// 'X-Parse-Session-Token': token
+				}
+			})
+		});
+	});
 
-  return getClient();
+	return getClient();
 };
 
 export default serverClient;
