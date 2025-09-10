17bc87fe6bb285c701e9993602226c665f43a0ed
Patrick Hanke <ph@patrickhanke.net>
2025-09-10 09:36:37 +0200

    ud

--- apps/patstore/app/(admin)/layout.tsx
+++ apps/patstore/app/(admin)/layout.tsx
@@ -1,1 +1,47 @@
-export { AdminLayout as default } from "@repo/modules";
+import React from "react";
+import "@repo/styles/global";
+import "@repo/styles/layout";
+// import { PatstoreProject } from "@repo/types";
+import SiteHeader from "./content/SiteHeader";
+
+
+
+export default async function RootLayout({
+	children
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="de">
+			<body>
+				<div className={"layout"}>
+					{/* <AdminLayoutContext */}
+						projects={[]}
+					>
+						{/* <RenderSidebar
+							menuItems={data.objects.findProject.results.map(
+								(project: PatstoreProject) => ({
+									label: project.name,
+									icon: undefined,
+									value: `/projects/${project.objectId}`
+								})
+							)}
+						/> */}
+
+						<div className={"main_content"} id="main_content">
+							<div
+								className={"content_container"}
+								id="page_content"
+							>
+								<SiteHeader />
+								<div className={"content"} id="content">
+									{children}
+								</div>
+							</div>
+						</div>
+					{/* </AdminLayoutContext> */}
+				</div>
+			</body>
+		</html>
+	);
+}
