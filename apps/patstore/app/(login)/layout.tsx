6dfb807665a4832205fd8e8dfbe8892e3216eb29
Patrick Hanke <ph@patrickhanke.net>
2025-11-09 09:49:04 +0100

    ud chakra context

--- apps/patstore/app/(login)/layout.tsx
+++ apps/patstore/app/(login)/layout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "@repo/styles/global";
 import "./styles.scss";
 import Framework from "./content/Framework";
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import { chakraConfig } from "@repo/provider";
 
 export const metadata = {
   title: "Patstore Login",
@@ -17,7 +19,9 @@ export default async function RootLayout({
     <html lang="de">
       <body className={"login_layout"}>
         <Framework />
-        <div className="login_content">{children}</div>
+        <ChakraProvider value={createSystem(defaultConfig, chakraConfig)}>
+          <div className="login_content">{children}</div>
+        </ChakraProvider>
       </body>
     </html>
   );
