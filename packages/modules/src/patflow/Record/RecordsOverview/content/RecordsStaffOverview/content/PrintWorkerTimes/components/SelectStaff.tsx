c30c06dd5a02868bfcdd0cb95aa193aeeaf51e2f
Patrick Hanke <ph@patrickhanke.net>
2026-04-28 07:57:27 +0200

    remove user queries

--- packages/modules/src/patflow/Record/RecordsOverview/content/RecordsStaffOverview/content/PrintWorkerTimes/components/SelectStaff.tsx
+++ packages/modules/src/patflow/Record/RecordsOverview/content/RecordsStaffOverview/content/PrintWorkerTimes/components/SelectStaff.tsx
@@ -2,30 +2,13 @@ import { FC, useMemo } from "react";
 import { DisplayWorker, ElementSelectInterface, SelectElement } from "@repo/ui";
 import { SelectStaffProps } from "../types";
 import { Worker } from "@repo/types";
-import { useFindDataSecure } from "@repo/provider";
+import { useDataStore } from "@repo/provider";
 
 const SelectStaff: FC<SelectStaffProps> = ({
 	selectedWorker,
 	setSelectedWorker
 }) => {
-	const { data: staffData } = useFindDataSecure({
-		objectName: "User",
-		fields: [
-			"objectId",
-			"first_name",
-			"last_name",
-			"is_worker",
-			"portrait { name url }",
-			"color",
-			"time_settings",
-			"number",
-			"data",
-			"role { objectId name type color }"
-		],
-		filters: [{ key: "is_worker", value: true, operator: "equalTo" }],
-		order: "last_name_DESC",
-		useMasterKey: true
-	});
+	const { workers: staffData } = useDataStore();
 
 	const elements = useMemo(() => {
 		const workerOptionsArray: SelectElement[] = [];
