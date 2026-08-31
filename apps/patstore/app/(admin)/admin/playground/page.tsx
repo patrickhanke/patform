caa9cd41589bbd817d568e4735ebd3228d9a171e
Patrick Hanke <ph@patrickhanke.net>
2026-08-31 12:40:42 +0200

    Update page.tsx

--- apps/patstore/app/(admin)/admin/playground/page.tsx
+++ apps/patstore/app/(admin)/admin/playground/page.tsx
@@ -1,1 +1,11 @@
-export { Playground as default } from "@repo/modules";
+// export { Playground as default } from "@repo/modules";
+
+const Playground = () => {
+	return <div>
+		<p>
+			Export Playground in Admin/index.js  
+		</p>
+	</div>;
+};
+
+export default Playground;
